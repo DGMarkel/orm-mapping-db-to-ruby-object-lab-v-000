b3694acde4b04dfec4bea1dc11c8b4430851c98c
David Markel <davidgmarkel@gmail.com>
2018-04-27 21:15:29 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,11 +4,9 @@ class Student
 
   def self.new_from_db(row)
     student = self.new
-    binding.pry
     student.id = row[0]
     student.name = row[1]
     student.grade = row[2]
-    student.save
   end
 
   def self.all
